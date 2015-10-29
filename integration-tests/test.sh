0177f1e75b58b2ca5e51a51a1b0fae5388aa92a0
Ahmed Elsabbahy <elsabbahyahmed@yahoo.com>
2015-10-29 07:20:32 -0400

    Some initial thoughts

--- integration-tests/test.sh
+++ integration-tests/test.sh
@@ -1,37 +1,34 @@
 #!/usr/bin/env bash
 
-set -x
+set -xe
 cp ../release/goss-linux-amd64 goss/goss
 
-if ! docker images | grep aelsabbahy/goss_centos;then
-  docker build -t aelsabbahy/goss_centos .
-fi
-
-if ! docker ps | grep goss_int_test;then
-  if docker ps -a | grep goss_int_test;then
-    docker rm -vf goss_int_test
+for os in centos6 wheezy;do
+  if ! docker images | grep aelsabbahy/goss_$os;then
+    docker build -t aelsabbahy/goss_$os - < Dockerfile_$os
   fi
-  docker run --privileged -v $PWD/goss:/tmp/goss  -d --name goss_int_test aelsabbahy/goss_centos /sbin/init
-  # Give httpd time to start up
-  sleep 10
-fi
 
-out=$(docker exec goss_int_test bash -c 'time /tmp/goss/goss -g /tmp/goss/goss.json validate')
-echo "$out"
+  if ! docker ps | grep goss_int_test_$os;then
+    if docker ps -a | grep goss_int_test_$os;then
+      docker rm -vf goss_int_test_$os
+    fi
+    docker run --privileged -v $PWD/goss:/tmp/goss  -d --name goss_int_test_$os aelsabbahy/goss_$os /sbin/init
+    # Give httpd time to start up
+    sleep 10
+  fi
 
-grep -q 'Count: 37 failed: 0' <<<"$out"
-exit_code=$?
+  out=$(docker exec goss_int_test_$os bash -c 'time /tmp/goss/goss -g /tmp/goss/goss.json validate')
+  echo "$out"
 
-docker exec goss_int_test bash -c 'time /tmp/goss/generate_goss.sh > /dev/null'
+  grep -q 'Count: 37 failed: 0' <<<"$out"
 
-docker exec goss_int_test bash -c 'diff -u /tmp/goss/goss-expected.json /tmp/goss/goss-generated.json'
-exit_code=$(($exit_code+$?))
+  docker exec goss_int_test_$os bash -c 'time /tmp/goss/generate_goss.sh > /dev/null'
 
-docker exec goss_int_test bash -c 'diff -u /tmp/goss/goss-aa-expected.json /tmp/goss/goss-aa-generated.json'
-exit_code=$(($exit_code+$?))
+  docker exec goss_int_test_$os bash -c 'diff -u /tmp/goss/goss-expected.json /tmp/goss/goss-generated.json'
 
-docker exec goss_int_test bash -c 'diff -u /tmp/goss/goss-expected.json <(/tmp/goss/goss -g /tmp/goss/goss-render.json render)'
-exit_code=$(($exit_code+$?))
-#docker rm -vf goss_int_test
+  docker exec goss_int_test_$os bash -c 'diff -u /tmp/goss/goss-aa-expected.json /tmp/goss/goss-aa-generated.json'
 
-exit $exit_code
+  docker exec goss_int_test_$os bash -c 'diff -u /tmp/goss/goss-expected.json <(/tmp/goss/goss -g /tmp/goss/goss-render.json render)'
+
+  #docker rm -vf goss_int_test_$os
+done
