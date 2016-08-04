bff9fbc24ca76df4c30a67e155bd7836e43062b9
Ahmed Elsabbahy <elsabbahyahmed@yahoo.com>
2016-08-03 22:28:28 -0400

    Add install.sh real

--- install.sh
+++ install.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+{
 set -e
 
-echo "hello world"
+LATEST="v0.2.2"
+GOSS_VER=${GOSS_VER:-$LATEST}
+
+arch=""
+if [ "$(uname -m)" = "x86_64" ]; then
+    arch="amd64"
+else
+    arch="386"
+fi
+
+url="https://github.com/aelsabbahy/goss/releases/download/$GOSS_VER/goss-linux-$arch"
+
+echo "Downloading $url"
+curl -sSL "https://github.com/aelsabbahy/goss/releases/download/$GOSS_VER/goss-linux-$arch" -o /usr/local/bin/goss
+chmod +rx /usr/local/bin/goss
+echo "Goss $GOSS_VER has been installed at /usr/local/bin/goss"
+echo "goss --version"
+/usr/local/bin/goss --version
+}
