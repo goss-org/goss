155bac9f4f1de2466cffe6efd5f6faa050efb582
Tim Birkett <tim@birkett-bros.com>
2016-08-26 16:06:51 +0100

    Add tap output to Nagios long output

--- outputs/nagios.go
+++ outputs/nagios.go
@@ -12,10 +12,12 @@ type Nagios struct{}
 
 func (r Nagios) Output(w io.Writer, results <-chan []resource.TestResult, startTime time.Time) (exitCode int) {
 	var testCount, failed, skipped int
+	var summary map[int]string
 	for resultGroup := range results {
 		for _, testResult := range resultGroup {
 			switch testResult.Result {
 			case resource.FAIL:
+				summary[failed] = "not ok " + strconv.Itoa(testCount+1) + " - " + humanizeResult2(testResult) + "\n"
 				failed++
 			case resource.SKIP:
 				skipped++
@@ -27,6 +29,9 @@ func (r Nagios) Output(w io.Writer, results <-chan []resource.TestResult, startT
 	duration := time.Since(startTime)
 	if failed > 0 {
 		fmt.Fprintf(w, "GOSS CRITICAL - Count: %d, Failed: %d, Skipped: %d, Duration: %.3fs\n", testCount, failed, skipped, duration.Seconds())
+		for i := 0; i < failed; i++ {
+			fmt.Fprintf(w, "%s", summary[i])
+		}
 		return 2
 	}
 	fmt.Fprintf(w, "GOSS OK - Count: %d, Failed: %d, Skipped: %d, Duration: %.3fs\n", testCount, failed, skipped, duration.Seconds())
