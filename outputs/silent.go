2ea032e2dcf996b2b80ead79893ef62832a9cc06
Daniel Strong <dstrong@glyx.co.uk>
2017-03-28 12:06:32 +0100

    Add a silent output
    When using goss as a helathchecking endpoint, it may be desirable to
    suppress verbose test output so that potential malicious entities can't
    scrape healthcheck endpoints for system information.
    
    Add the `silent` output, which simply checks whether any tests have
    failed and returns 1 or 0 accordingly, without printing any output.

---
+++ outputs/silent.go
@@ -0,0 +1,31 @@
+package outputs
+
+import (
+	"io"
+	"time"
+
+	"github.com/aelsabbahy/goss/resource"
+)
+
+type Silent struct{}
+
+func (r Silent) Output(w io.Writer, results <-chan []resource.TestResult, startTime time.Time) (exitCode int) {
+	var failed int
+	for resultGroup := range results {
+		for _, testResult := range resultGroup {
+			switch testResult.Result {
+			case resource.FAIL:
+				failed++
+			}
+		}
+	}
+
+	if failed > 0 {
+		return 1
+	}
+	return 0
+}
+
+func init() {
+	RegisterOutputer("silent", &Silent{})
+}
