0de910042c5f8d1adef4f2d007f8d329808011bd
Alexandre Carlton <AlexandreCarlton@users.noreply.github.com>
2017-12-20 08:26:34 +1100

    Add MTU to interface test (#288)
    * Add mtu for interface test
    
    References #287
    
    * Add mtu to shared integration test
    
    The default MTU in Docker is 1500 bytes, so this seems like a reasonable
    value against which to test the MTU feature.
    
    References #287
    
    * Update success counts in test.sh
    
    References #287

--- resource/interface.go
+++ resource/interface.go
@@ -11,6 +11,7 @@ type Interface struct {
 	Name   string  `json:"-" yaml:"-"`
 	Exists matcher `json:"exists" yaml:"exists"`
 	Addrs  matcher `json:"addrs,omitempty" yaml:"addrs,omitempty"`
+	MTU    matcher `json:"mtu,omitempty" yaml:"mtu,omitempty"`
 }
 
 func (i *Interface) ID() string      { return i.Name }
@@ -32,6 +33,9 @@ func (i *Interface) Validate(sys *system.System) []TestResult {
 	if i.Addrs != nil {
 		results = append(results, ValidateValue(i, "addrs", i.Addrs, sysInterface.Addrs, skip))
 	}
+	if i.MTU != nil {
+		results = append(results, ValidateValue(i, "mtu", i.MTU, sysInterface.MTU, skip))
+	}
 	return results
 }
 
@@ -45,6 +49,11 @@ func NewInterface(sysInterface system.Interface, config util.Config) (*Interface
 	if !contains(config.IgnoreList, "addrs") {
 		if addrs, err := sysInterface.Addrs(); err == nil {
 			i.Addrs = addrs
+		}
+	}
+	if !contains(config.IgnoreList, "mtu") {
+		if mtu, err := sysInterface.MTU(); err == nil {
+			i.MTU = mtu
 		}
 	}
 	return i, nil
