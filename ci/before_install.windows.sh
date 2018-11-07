e28524628d535e28db7ebeee94059a6f49510727
Peter Mounce <petermounce@improbable.io>
2018-11-07 00:59:03 +0000

    add windows

---
+++ ci/before_install.windows.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# -ET: propagate DEBUG/RETURN/ERR traps to functions and subshells
+# -e: exit on unhandled error
+# pipefail: any failure in a pipe causes the pipe to fail
+set -eET -o pipefail
+[[ -n "${DEBUG:-}" ]] && set -x
+if ! cd "$(dirname "${BASH_SOURCE[0]}")/.."; then
+  echo -e "Failed to cd to repository root"
+  return 1
+fi
+
+curl -L https://github.com/Masterminds/glide/releases/download/0.10.2/glide-0.10.2-windows-amd64.zip --output glide.zip
+unzip glide.zip
+go get -u golang.org/x/lint/golint
