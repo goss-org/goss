15de5cdf4d0bd5b885399f33b2053056509df80d
Peter Mounce <petermounce@improbable.io>
2018-11-10 02:33:48 +0000

    what's going on

--- ci/before_install.osx.sh
+++ ci/before_install.osx.sh
@@ -6,7 +6,8 @@
 # -e: exit on unhandled error
 # pipefail: any failure in a pipe causes the pipe to fail
 set -eET -o pipefail
-[[ -n "${DEBUG:-}" ]] && set -x
+# [[ -n "${DEBUG:-}" ]] && set -x
+set -x
 if ! cd "$(dirname "${BASH_SOURCE[0]}")/.."; then
   echo -e "Failed to cd to repository root"
   return 1
@@ -18,5 +19,6 @@ sudo launchctl unload -w /System/Library/LaunchDaemons/org.apache.httpd.plist
 
 brew install httpd
 # brew's httpd listens on 8080; adjust to make tests pass.
+cat /usr/local/etc/httpd/httpd.conf
 sudo sed -i 's/Listen 8080/Listen 80/' /usr/local/etc/httpd/httpd.conf
 sudo brew services start httpd
