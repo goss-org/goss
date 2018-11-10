4686af9300025c7a5b40d5b5e169d99b283e1711
Peter Mounce <petermounce@improbable.io>
2018-11-10 20:08:55 +0000

    Try and see output

--- integration-tests/test.sh
+++ integration-tests/test.sh
@@ -47,7 +47,9 @@ trap "rv=\$?; docker rm -vf $id; exit \$rv" INT TERM EXIT
 [[ $os != "arch" ]] && docker_exec "/goss/$os/goss-linux-$arch" -g "/goss/goss-wait.yaml" validate -r 10s -s 100ms && sleep 1
 
 #out=$(docker exec "$container_name" bash -c "time /goss/$os/goss-linux-$arch -g /goss/$os/goss.yaml validate")
+set +e # this can fail, but if it does, no output is shown.
 out=$(docker_exec "/goss/$os/goss-linux-$arch" --vars "/goss/vars.yaml" -g "/goss/$os/goss.yaml" validate)
+set -e
 echo "$out"
 
 if [[ $os == "arch" ]]; then
