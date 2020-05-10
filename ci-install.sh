1e33748c3a67206e3c97959545de6696db1adb0f
Peter Mounce <petermounce@improbable.io>
2020-05-10 11:53:23 +0100

    oh, Windows

--- ci-install.sh
+++ ci-install.sh
@@ -5,8 +5,12 @@ goos="${TRAVIS_OS_NAME:?"No value for TRAVIS_OS_NAME. This is meant to be run in
 if [[ "${goos}" == "osx" ]]; then
   goos="darwin"
 fi
+extension=""
+if [[ "${goos}" == "windows" ]]; then
+  extension=".exe"
+fi
 
 go get -u golang.org/x/lint/golint
 
-curl -L "https://codeclimate.com/downloads/test-reporter/test-reporter-latest-${goos}-amd64" > ./cc-test-reporter
-chmod +x ./cc-test-reporter
+curl -L "https://codeclimate.com/downloads/test-reporter/test-reporter-latest-${goos}-amd64${extension}" > "./cc-test-reporter${extension}"
+chmod +x "./cc-test-reporter${extension}"
