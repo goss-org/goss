670cdd471983490e099d181adc8ab79b305e6e50
Peter Mounce <petermounce@improbable.io>
2020-05-10 13:56:54 +0100

    windows

--- ci/go-fmt.sh
+++ ci/go-fmt.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+os_name="${1:?"No value from TRAVIS_OS_NAME in 1st arg. This is meant to be run in Travis CI, see also https://docs.travis-ci.com/user/environment-variables/#convenience-variables"}"
+
 # gofmt must be on PATH
 command -v gofmt
+
+if [[ "${os_name}" == "windows" ]]; then
+  echo "Skipping go-fmt on Windows because line-endings cause every file to need formatting."
+  echo "Linux is treated as authoritative."
+  echo "Exiting 0..."
+  exit 0
+fi
 
 fmt="$(go fmt github.com/aelsabbahy/goss/...)"
 
