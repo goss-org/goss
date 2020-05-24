b1f60f57f6a8685cdaa7a42fc9cdfd1dd1b3a125
Peter Mounce <pete@neverrunwithscissors.com>
2020-05-24 16:02:58 +0100

    bash and debug

--- integration-tests/run-tests-alpha.sh
+++ integration-tests/run-tests-alpha.sh
@@ -5,7 +5,7 @@ os_name="${1:?Enter the OS name you want to run (windows|darwin)}"
 
 repo_root="$(git rev-parse --show-toplevel)"
 export GOSS_BINARY="${repo_root}/release/goss-alpha-${os_name}-amd64"
-echo "Using ${GOSS_BINARY}"
+echo "Using: '${GOSS_BINARY}', cwd: '$(pwd)'"
 readarray -t goss_test_files < <(find integration-tests -type f -name "*.goss.yaml" | grep "${os_name}" | sort | uniq)
 
 export GOSS_USE_ALPHA=1
