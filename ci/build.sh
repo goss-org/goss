cfa15840bef3a4c1ebb1cb15fa632adf8a6ed3c5
Peter Mounce <pete@neverrunwithscissors.com>
2020-05-25 11:03:21 +0100

    actually building the binary helps when running it!

--- ci/build.sh
+++ ci/build.sh
@@ -6,13 +6,11 @@ os_name="${1:?"No value from TRAVIS_OS_NAME in 1st arg. This is meant to be run 
 # darwin & windows do not support integration-testing approach via docker, so on those, just run fast tests.
 # linux runs all tests; unit and integration.
 if [[ "${os_name}" == "osx" ]]; then
-  make test-short-all
-  pwd
+  make test-short-all release/goss-alpha-darwin-amd64
   # darwin is the GOOS value which is easier to work with
   integration-tests/run-tests-alpha.sh "darwin"
 elif [[ "${os_name}" == "windows" ]]; then
-  make test-short-all
-  pwd
+  make test-short-all release/goss-alpha-windows-amd64
   integration-tests/run-tests-alpha.sh "windows"
 else
   make all
