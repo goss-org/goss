ed0c66558c3db14ebe05ffd6ad35ec1f57335e22
Peter Mounce <pete@neverrunwithscissors.com>
2020-06-15 21:07:40 +0100

    oops

--- ci/install.sh
+++ ci/install.sh
@@ -5,7 +5,7 @@ os_name="$(go env GOOS)"
 
 go get -u golang.org/x/lint/golint
 
-if [[ "${goos}" != "windows" ]]; then
-  curl -L "https://codeclimate.com/downloads/test-reporter/test-reporter-latest-${goos}-amd64" > "./cc-test-reporter"
+if [[ "${os_name}" != "windows" ]]; then
+  curl -L "https://codeclimate.com/downloads/test-reporter/test-reporter-latest-${os_name}-amd64" > "./cc-test-reporter"
   chmod +x "./cc-test-reporter"
 fi
