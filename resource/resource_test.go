83a944b0159581d6195d621b5e4a7e52b8ae6bb9
Peter Mounce <pete@neverrunwithscissors.com>
2020-08-27 15:17:47 +0100

    almost there

--- resource/resource_test.go
+++ resource/resource_test.go
@@ -2,16 +2,47 @@ package resource
 
 import (
 	"fmt"
-	"reflect"
+	"go/types"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/tools/go/packages"
 )
 
 func TestResourcesAreInitialised(t *testing.T) {
 	assert.NotEmpty(t, Resources())
 
-	var i Resource
-	it := reflect.TypeOf(i)
-	fmt.Printf("it: %v", it)
+	loaded, err := packages.Load(&packages.Config{
+		Mode: packages.NeedTypes | packages.NeedTypesInfo | packages.NeedSyntax,
+	}, "github.com/aelsabbahy/goss/resource")
+	require.NoError(t, err)
+
+	found := map[string]types.TypeAndValue{}
+	for _, pkg := range loaded {
+		for t, typ := range pkg.TypesInfo.Types {
+			if !typ.IsType() || typ.IsNil() || typ.IsBuiltin() {
+				continue
+			}
+			if !strings.HasPrefix(typ.Type.String(), "github.com/aelsabbahy/goss/resource.") {
+				continue
+			}
+			if !strings.HasPrefix(typ.Type.Underlying().String(), "struct") {
+				continue
+			}
+			found[strings.ToLower(fmt.Sprintf("%s", t))] = typ
+		}
+	}
+	foundAsArray := []string{}
+	for name := range found {
+		foundAsArray = append(foundAsArray, name)
+	}
+	actualAsArray := []string{}
+	for name := range Resources() {
+		actualAsArray = append(actualAsArray, name)
+	}
+
+	// subset, not equal, because 'found' contains some extraneous matches that are harder to filter out.
+	assert.Subset(t, foundAsArray, actualAsArray)
 }
