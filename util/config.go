91a0583b35fa9de0150d159d7e510fac9515cddc
Peter Mounce <petermounce@improbable.io>
2020-09-01 21:16:16 +0100

    lint; outdent

--- util/config.go
+++ util/config.go
@@ -288,7 +288,6 @@ func IsValueInList(value string, list []string) bool {
 func GetType(myvar interface{}) string {
 	if t := reflect.TypeOf(myvar); t.Kind() == reflect.Ptr {
 		return "*" + t.Elem().Name()
-	} else {
-		return t.Name()
 	}
+	return t.Name()
 }
