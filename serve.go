b3b07d6d0bffcb947f5ba3094e6cfb632218f64f
Peter Mounce <pete@neverrunwithscissors.com>
2020-10-18 15:18:30 +0100

    Refactor serve toward caching results instead of rendered
    Step 1: Handle http parts completely within the handler func.
    Step 2: Rename res -> runResult for clarity

--- serve.go
+++ serve.go
@@ -54,10 +54,18 @@ func newHealthHandler(c *util.Config) (*healthHandler, error) {
 	return health, nil
 }
 
-type res struct {
-	body       bytes.Buffer
-	statusCode int
+type runResult struct {
+	body     bytes.Buffer
+	exitCode int
 }
+
+func (rr runResult) toHTTPStatus() int {
+	if rr.exitCode == 0 {
+		return http.StatusOK
+	}
+	return http.StatusServiceUnavailable
+}
+
 type healthHandler struct {
 	c             *util.Config
 	gossConfig    GossConfig
@@ -80,20 +88,23 @@ func (h healthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Printf("%v: requesting health probe", r.RemoteAddr)
 	resp := h.processAndEnsureCached(negotiatedContentType, outputer)
 	w.Header().Set(http.CanonicalHeaderKey("Content-Type"), negotiatedContentType)
-	w.WriteHeader(resp.statusCode)
+	statusCode := resp.toHTTPStatus()
+	w.WriteHeader(statusCode)
 	logBody := ""
-	if resp.statusCode != http.StatusOK {
+	if statusCode != http.StatusOK {
+		// if there are any test failures, log all the details since machine state is volatile
+		// and highly subject to intermittent failures compared to unit-tests.
 		logBody = " - " + resp.body.String()
 	}
 	resp.body.WriteTo(w)
-	log.Printf("%v: status %d%s", r.RemoteAddr, resp.statusCode, logBody)
+	log.Printf("%v: status %d%s", r.RemoteAddr, statusCode, logBody)
 }
 
-func (h healthHandler) processAndEnsureCached(negotiatedContentType string, outputer outputs.Outputer) res {
-	cacheKey := fmt.Sprintf("res:%s", negotiatedContentType)
+func (h healthHandler) processAndEnsureCached(negotiatedContentType string, outputer outputs.Outputer) runResult {
+	cacheKey := fmt.Sprintf("runResult:%s", negotiatedContentType)
 	tmp, found := h.cache.Get(cacheKey)
 	if found {
-		return tmp.(res)
+		return tmp.(runResult)
 	}
 
 	h.gossMu.Lock()
@@ -101,33 +112,20 @@ func (h healthHandler) processAndEnsureCached(negotiatedContentType string, outp
 	tmp, found = h.cache.Get(cacheKey)
 	if found {
 		log.Printf("Returning cached[%s].", cacheKey)
-		return tmp.(res)
+		return tmp.(runResult)
 	}
 
 	log.Printf("Stale cache[%s], running tests", cacheKey)
-	resp := h.runValidate(outputer)
-	h.cache.SetDefault(cacheKey, resp)
-	return resp
+	rr := h.runValidate(outputer)
+	h.cache.SetDefault(cacheKey, rr)
+	return rr
 }
 
-func (h healthHandler) runValidate(outputer outputs.Outputer) res {
+func (h healthHandler) runValidate(outputer outputs.Outputer) runResult {
 	h.sys = system.New(h.c.PackageManager)
 	iStartTime := time.Now()
-	out := validate(h.sys, h.gossConfig, h.maxConcurrent)
-	var b bytes.Buffer
-	outputConfig := util.OutputConfig{
-		FormatOptions: h.c.FormatOptions,
-	}
-	exitCode := outputer.Output(&b, out, iStartTime, outputConfig)
-	resp := res{
-		body: b,
-	}
-	if exitCode == 0 {
-		resp.statusCode = http.StatusOK
-	} else {
-		resp.statusCode = http.StatusServiceUnavailable
-	}
-	return resp
+	validateResult := validate(h.sys, h.gossConfig, h.maxConcurrent)
+	return h.renderBody(validateResult, outputer, iStartTime)
 }
 
 const (
@@ -168,11 +166,15 @@ func (h healthHandler) responseContentType(outputName string) string {
 	return fmt.Sprintf("%s%s", mediaTypePrefix, outputName)
 }
 
-func (h healthHandler) renderBody(results <-chan []resource.TestResult, outputer outputs.Outputer) (int, bytes.Buffer) {
+func (h healthHandler) renderBody(results <-chan []resource.TestResult, outputer outputs.Outputer, startTime time.Time) runResult {
 	outputConfig := util.OutputConfig{
 		FormatOptions: h.c.FormatOptions,
 	}
 	var b bytes.Buffer
-	exitCode := outputer.Output(&b, results, time.Now(), outputConfig)
-	return exitCode, b
+	exitCode := outputer.Output(&b, results, startTime, outputConfig)
+	rr := runResult{
+		body:     b,
+		exitCode: exitCode,
+	}
+	return rr
 }
