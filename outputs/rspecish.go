61f12a304d96199a59f9e6fa8d6324f503351504
Olivier Bourdon <olivier.bourdon@squarescale.com>
2020-10-22 21:54:17 +0200

    Add log traces to JSON and text outputs

--- outputs/rspecish.go
+++ outputs/rspecish.go
@@ -3,6 +3,8 @@ package outputs
 import (
 	"fmt"
 	"io"
+	"log"
+	"strings"
 	"time"
 
 	"github.com/goss-org/goss/resource"
@@ -26,12 +28,36 @@ func (r Rspecish) Output(w io.Writer, results <-chan []resource.TestResult,
 		for _, testResult := range resultGroup {
 			switch testResult.Result {
 			case resource.SUCCESS:
+				log.Printf("[TRACE] SUCCESS: %s => %s (%s %+v %+v) [%.02f]",
+					testResult.ResourceType,
+					testResult.ResourceId,
+					testResult.Property,
+					testResult.Expected,
+					testResult.Found,
+					testResult.Duration.Seconds(),
+				)
 				fmt.Fprintf(w, green("."))
 			case resource.SKIP:
+				log.Printf("[TRACE] SKIP: %s => %s (%s %+v %+v) [%.02f]",
+					testResult.ResourceType,
+					testResult.ResourceId,
+					testResult.Property,
+					testResult.Expected,
+					testResult.Found,
+					testResult.Duration.Seconds(),
+				)
 				fmt.Fprintf(w, yellow("S"))
 				failedOrSkippedGroup = append(failedOrSkippedGroup, testResult)
 				skipped++
 			case resource.FAIL:
+				log.Printf("[WARN] FAIL: %s => %s (%s %+v %+v) [%.02f]",
+					testResult.ResourceType,
+					testResult.ResourceId,
+					testResult.Property,
+					testResult.Expected,
+					testResult.Found,
+					testResult.Duration.Seconds(),
+				)
 				fmt.Fprintf(w, red("F"))
 				failedOrSkippedGroup = append(failedOrSkippedGroup, testResult)
 				failed++
@@ -46,9 +72,13 @@ func (r Rspecish) Output(w io.Writer, results <-chan []resource.TestResult,
 	fmt.Fprint(w, "\n\n")
 	fmt.Fprint(w, failedOrSkippedSummary(failedOrSkipped))
 
-	fmt.Fprint(w, summary(startTime, testCount, failed, skipped))
+	outstr := summary(startTime, testCount, failed, skipped)
+	fmt.Fprint(w, outstr)
+	resstr := strings.ReplaceAll(outstr, "\n", " ")
 	if failed > 0 {
+		log.Printf("[WARN] FAIL SUMMARY: %s", resstr)
 		return 1
 	}
+	log.Printf("[INFO] OK SUMMARY: %s", resstr)
 	return 0
 }
