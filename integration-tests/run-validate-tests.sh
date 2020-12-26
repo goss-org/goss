83e1aa9102cc09d4a99651a740e27c1ba76fc70a
Peter Mounce <petermounce@improbable.io>
2020-12-26 17:13:03 +0000

    set expectations for future-us re: macOS+Windows vs linux

--- integration-tests/run-validate-tests.sh
+++ integration-tests/run-validate-tests.sh
@@ -9,6 +9,15 @@ IFS='- ' read -r -a segments <<< "${platform_spec}"
 os="${segments[0]}"
 arch="${segments[1]}"
 
+if [[ "${os}" == "linux" ]]; then
+  echo "OS is ${os}. This script is not for running tests on the different flavours of linux."
+  echo "Linux is exercised via the integration-tests/test.sh currently, because linux can be"
+  echo "verified via docker containers; macOS and Windows cannot."
+  echo "This script is for macOS and Windows, and runs tests that are expected to pass on"
+  echo "Travis-CI provided images, running nakedly (no containerisation) on the hosts there."
+  exit 1
+fi
+
 repo_root="$(git rev-parse --show-toplevel)"
 export GOSS_BINARY="${repo_root}/release/goss-${platform_spec}"
 log_info "Using: '${GOSS_BINARY}', cwd: '$(pwd)', os: ${os}"
