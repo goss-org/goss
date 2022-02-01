bbaff9873cbe44e531b8b76c6499dc885d5fdf9a
Jan-Otto Kröpke <joe@adorsys.de>
2022-02-01 21:57:32 +0100

    Remove curl dependency from install.sh
    Additionally:
    - Lint shell script with shellcheck
    - Format shell script with shfmt

--- install.sh
+++ install.sh
@@ -1,48 +1,67 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 {
-set -e
+  set -e
 
-LATEST_URL="https://github.com/aelsabbahy/goss/releases/latest"
-LATEST_EFFECTIVE=$(curl -s -L -o /dev/null ${LATEST_URL} -w '%{url_effective}')
-LATEST=${LATEST_EFFECTIVE##*/}
-
-DGOSS_VER=$GOSS_VER
-
-if [ -z "$GOSS_VER" ]; then
-    GOSS_VER=${GOSS_VER:-$LATEST}
-    DGOSS_VER='master'
-fi
-if [ -z "$GOSS_VER" ]; then
-    echo "ERROR: Could not automatically detect latest version, set GOSS_VER env var and re-run"
+  if command -v curl >/dev/null; then
+    HTTP_GET="curl -sSfL"
+  elif command -v wget >/dev/null; then
+    HTTP_GET="wget -qO-"
+  else
+    echo "[ERROR] This script needs wget or curl to be installed."
     exit 1
-fi
-GOSS_DST=${GOSS_DST:-/usr/local/bin}
-INSTALL_LOC="${GOSS_DST%/}/goss"
-DGOSS_INSTALL_LOC="${GOSS_DST%/}/dgoss"
-touch "$INSTALL_LOC" || { echo "ERROR: Cannot write to $GOSS_DST set GOSS_DST elsewhere or use sudo"; exit 1; }
+  fi
 
-arch=""
-if [ "$(uname -m)" = "x86_64" ]; then
+  # shellcheck disable=SC2153
+  DGOSS_VER="${GOSS_VER}"
+
+  GOSS_DST=${GOSS_DST:-/usr/local/bin}
+  INSTALL_LOC="${GOSS_DST%/}/goss"
+  DGOSS_INSTALL_LOC="${GOSS_DST%/}/dgoss"
+
+  touch "$INSTALL_LOC" || {
+    echo "ERROR: Cannot write to $GOSS_DST set GOSS_DST elsewhere or use sudo"
+    exit 1
+  }
+
+  case "$(uname -m)" in
+  x86_64)
     arch="amd64"
-elif [ "$(uname -m)" = "aarch64" ]; then
+    ;;
+  aarch64)
     arch="arm"
-else
+    ;;
+  *)
     arch="386"
-fi
+    ;;
+  esac
 
-url="https://github.com/aelsabbahy/goss/releases/download/$GOSS_VER/goss-linux-$arch"
+  if [ -n "$GOSS_VER" ]; then
+    GOSS_URL="https://github.com/aelsabbahy/goss/releases/download/$GOSS_VER/goss-linux-$arch"
+    DGOSS_URL="https://raw.githubusercontent.com/aelsabbahy/goss/$DGOSS_VER/extras/dgoss/dgoss"
+  else
+    GOSS_URL="https://github.com/aelsabbahy/goss/releases/latest/download/goss-linux-$arch"
+    DGOSS_URL="https://raw.githubusercontent.com/aelsabbahy/goss/master/extras/dgoss/dgoss"
+  fi
 
-echo "Downloading $url"
-curl -L "$url" -o "$INSTALL_LOC"
-chmod +rx "$INSTALL_LOC"
-echo "Goss $GOSS_VER has been installed to $INSTALL_LOC"
-echo "goss --version"
-"$INSTALL_LOC" --version
+  echo "Downloading ${GOSS_URL}"
+  if ! $HTTP_GET "$GOSS_URL" > "$INSTALL_LOC"; then
+    echo "ERROR: Cannot download goss from $GOSS_URL"
+    exit 1
+  fi
 
-dgoss_url="https://raw.githubusercontent.com/aelsabbahy/goss/$DGOSS_VER/extras/dgoss/dgoss"
-echo "Downloading $dgoss_url"
-curl -L "$dgoss_url" -o "$DGOSS_INSTALL_LOC"
-chmod +rx "$DGOSS_INSTALL_LOC"
-echo "dgoss $DGOSS_VER has been installed to $DGOSS_INSTALL_LOC"
+  chmod +rx "$INSTALL_LOC"
+
+  echo "Goss has been installed to $INSTALL_LOC"
+  echo "goss --version"
+  "$INSTALL_LOC" --version
+
+  echo "Downloading ${DGOSS_URL}"
+  if ! $HTTP_GET "$DGOSS_URL" > "$DGOSS_INSTALL_LOC"; then
+    echo "ERROR: Cannot download goss from $GOSS_URL"
+    exit 1
+  fi
+
+  chmod +rx "$DGOSS_INSTALL_LOC"
+  echo "dgoss has been installed to $DGOSS_INSTALL_LOC"
 }
