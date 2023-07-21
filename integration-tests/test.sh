54a77a9eb9452d1547234f3097f4e4103eb3385d
Ahmed Elsabbahy <aelsabbahy@users.noreply.github.com>
2023-07-21 10:21:44 -0700

    Update dependencies (#822)
    * Update dependencies
    
    * FIX: quick fix to remove CI dependency on httpbin.org

--- integration-tests/test.sh
+++ integration-tests/test.sh
@@ -30,10 +30,16 @@ docker_exec() {
 if docker ps -a | grep "$container_name";then
   docker rm -vf "$container_name"
 fi
+
+# Setup local httbin
+# FIXME: this is a quick hack to fix intermittent CI issues
+network=goss-test
+docker network create --driver bridge $network
+docker run -d --name httpbin --network $network kennethreitz/httpbin
 opts=(--env OS=$os --cap-add SYS_ADMIN -v "$PWD/goss:/goss" -d --name "$container_name" --security-opt seccomp:unconfined --security-opt label:disable)
-id=$(docker run "${opts[@]}" "aelsabbahy/goss_$os" /sbin/init)
+id=$(docker run "${opts[@]}" --network $network "aelsabbahy/goss_$os" /sbin/init)
 ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$id")
-trap "rv=\$?; docker rm -vf $id; exit \$rv" INT TERM EXIT
+trap "rv=\$?; docker rm -vf $id;docker rm -vf httpbin;docker network rm $network; exit \$rv" INT TERM EXIT
 # Give httpd time to start up, adding 1 second to see if it helps with intermittent CI failures
 [[ $os != "arch" ]] && docker_exec "/goss/$os/goss-linux-$arch" -g "/goss/goss-wait.yaml" validate -r 10s -s 100ms && sleep 1
 
