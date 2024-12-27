10d4b129d025a831e47d593c1479f276f8aa9cfc
Chad Thielen <chad.thielen@corteva.com>
2024-12-27 13:15:50 -0700

    save off changes

---
+++ system/service_windows.go
@@ -0,0 +1,63 @@
+package system
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/goss-org/goss/util"
+)
+
+type ServiceWindows struct {
+	service string
+}
+
+func NewServiceWindows(_ context.Context, service string, system *System, config util.Config) Service {
+	return &ServiceWindows{
+		service: service,
+	}
+}
+
+func (s *ServiceWindows) Service() string {
+	return s.service
+}
+
+func (s *ServiceWindows) Exists() (bool, error) {
+	if invalidService(s.service) {
+		return false, nil
+	}
+	cmd := util.NewCommand("systemctl", "-q", "list-unit-files", "--type=service")
+	cmd.Run()
+	if strings.Contains(cmd.Stdout.String(), fmt.Sprintf("%s.service", s.service)) {
+		return true, cmd.Err
+	}
+	return false, nil
+}
+
+func (s *ServiceWindows) Enabled() (bool, error) {
+	if invalidService(s.service) {
+		return false, nil
+	}
+	cmd := util.NewCommand("systemctl", "-q", "is-enabled", s.service)
+	cmd.Run()
+	if cmd.Status == 0 {
+		return true, cmd.Err
+	}
+	return false, nil
+}
+
+func (s *ServiceWindows) Running() (bool, error) {
+	if invalidService(s.service) {
+		return false, nil
+	}
+	cmd := util.NewCommand("systemctl", "-q", "is-active", s.service)
+	cmd.Run()
+	if cmd.Status == 0 {
+		return true, cmd.Err
+	}
+	return false, nil
+}
+
+func (s *ServiceWindows) RunLevels() ([]string, error) {
+	return nil, nil
+}
